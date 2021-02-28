10f35f2584291a9020d34d258140289c19b526f9
Robert <insayt@bk.ru>
2021-02-28 17:43:38 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// 1, 1, 2, 3, 5, 8, 13, 21
+
+lastNumber = 0;
+
+firstNumber = 1;
+
+for(let i = 0; i < 10 ; i++) {
+    prevNumber = lastNumber;
+    lastNumber = 
+
+
+ console.log(// 1, 1, 2, 3, 5, 8, 13, 21)
+ 
+}
